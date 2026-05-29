fda1cf8dd5ede3bc693d9f97f828f72aa2fb080e
MBKANERIYA <mbkaneriya1998@gmail.com>
2026-05-29 11:52:53 +0530

    Fix Vercel rewrite destinations: point to /api instead of /api/index.js (Vercel maps api/index.js to /api). Remove test api/hello.js

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-      message: "Hello from Vercel API!", 
-      url: req.url,
-      originalUrl: req.originalUrl,
-      headers: req.headers
-  })
-}
